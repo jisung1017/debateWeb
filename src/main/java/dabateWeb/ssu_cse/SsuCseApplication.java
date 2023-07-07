10ff67db018f9595689d8e0a9708b49c7f5e1984
박지성 <010jisung@gmail.com>
2023-07-07 15:47:18 +0900

    첫번째 프로젝트 파일

---
+++ src/main/java/dabateWeb/ssu_cse/SsuCseApplication.java
@@ -0,0 +1,13 @@
+package dabateWeb.ssu_cse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsuCseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsuCseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dabateWeb/ssu_cse/SsuCseApplicationTests.java
@@ -0,0 +1,13 @@
+package dabateWeb.ssu_cse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsuCseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
